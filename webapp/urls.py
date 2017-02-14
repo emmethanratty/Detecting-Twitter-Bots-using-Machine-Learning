b79164d3500ef3a473e2ed5d294b351d4c498bca
Emmet Hanratty <emmethanratty@hotmail.com>
2017-02-14 11:27:34 +0000

    Views added index

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'webapp'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^home/, views.home, name='home'),
+    #url(r'^index/', views.index, name='index'),
+    url(r'^home/', views.home, name='home'),
+]
--- webapp/views.py
+++ webapp/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Web App")
+    return render(request, 'webapp/index.html')
 
 def home(request):
     return HttpResponse("Home Page")
